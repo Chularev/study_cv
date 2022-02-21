523cb70cd1fedfc11983f64acfe28a7317c2d8ee
Chularev <chulgarev@gmail.com>
2022-02-21 21:39:59 +0700

    fixed bug

--- pytorch_framework/trainer.py
+++ pytorch_framework/trainer.py
@@ -44,7 +44,7 @@ class Trainer:
 
         with torch.inference_mode():
             metrics = self.metrics[self.phase].step(prediction, gpu_img_has_person, gpu_box)
-            for key in losses.keys():
+            for key in metrics.keys():
                 self.logger.add_scalar('Metric_{}/{}'.format(self.phase, key), metrics[key])
 
         return sum(losses.values())
