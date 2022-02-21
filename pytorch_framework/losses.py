5b5d73e2b11b0ed4f7d4fc1aff2b04103c1c6cde
Chularev <chulgarev@gmail.com>
2022-02-21 20:07:38 +0700

    refactoring

--- pytorch_framework/losses.py
+++ pytorch_framework/losses.py
@@ -1,4 +1,5 @@
 from torch import Tensor
+from typing import Dict
 import torch
 
 
@@ -9,11 +10,14 @@ class MyLoss:
         self.class_loss = torch.nn.BCELoss()
         self.bbox_loss = torch.nn.SmoothL1Loss()
 
-    def calc(self, prediction: Tensor, gpu_img_has_person: Tensor, gpu_box: Tensor) -> Tensor:
+    def calc(self, prediction: Tensor, gpu_img_has_person: Tensor, gpu_box: Tensor) -> Dict[str, Tensor]:
+        result: Dict[str, Tensor] = {}
 
         loss_value = self.class_loss(prediction['class'], gpu_img_has_person.type(torch.cuda.FloatTensor))
+        result['class_loss'] = loss_value
 
         indexes_with_label = (gpu_img_has_person == 1).nonzero(as_tuple=True)
         if len(indexes_with_label) > 0:
-            return loss_value + self.bbox_loss(prediction['bbox'][indexes_with_label], gpu_box[indexes_with_label])
-        return loss_value
+            result['bbox_loss'] = self.bbox_loss(prediction['bbox'][indexes_with_label], gpu_box[indexes_with_label])
+
+        return result
