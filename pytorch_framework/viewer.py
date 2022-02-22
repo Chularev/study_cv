5d9bed2098be3c26ccd504fbfd6988e35f8f730c
Chularev <chulgarev@gmail.com>
2022-02-23 03:49:55 +0700

    rerdo viewer

--- pytorch_framework/viewer.py
+++ pytorch_framework/viewer.py
@@ -13,36 +13,30 @@ class Viewer:
     def convert_from_image_to_cv2(self, img: Image) -> np.ndarray:
         return cv2.cvtColor(np.array(img), cv2.COLOR_RGB2BGR)
 
-    def print_img(self, img, target):
-        label = "Yes" if target['img_has_person'] == 1 else 'No'
-        if target['img_has_person'] == 1:
-            img = self.add_box(img, target)
-        plt.title(label)
-        plt.imshow(img)
-        plt.show()
-
     def convert_box(self, box, width, height):
-        return [math.ceil(box[0] * width), math.ceil(box[1]*height), math.ceil(box[2]*width), math.ceil(box[3]*height)]
+        return [math.ceil(box[0] * width), math.ceil(box[1] * height), math.ceil(box[2] * width),
+                math.ceil(box[3] * height)]
 
-    def add_box(self, img, target):
-        box = self.convert_box(target['box'], target['img_width'], target['img_height'])
+    def add_box(self, img, box, target, color):
+        box = self.convert_box(box, target['img_width'], target['img_height'])
 
         img = self.convert_from_image_to_cv2(img)
         img = cv2.rectangle(img, (box[0], box[1]), (box[2], box[3]),
-                              (255, 0, 0), 4)
+                            color, 4)
         return self.convert_from_cv2_to_image(img)
 
     def print_prediction(self, img, target, prediction):
-        self.print_img(img, target)
-        flag = torch.round(torch.sigmoid(prediction[0])) == 1
-        label = "Yes" if flag else 'No'
-        if flag:
-            box = self.convert_box(prediction[1:], target['img_width'], target['img_height'])
+        label = "Gt "
+        label += "Yes" if target['img_has_person'] == 1 else 'No'
+        if target['img_has_person'] == 1:
+            img = self.add_box(img, target['box'], target, (0, 255, 0))
 
-            img = self.convert_from_image_to_cv2(img)
-            img = cv2.rectangle(img, (box[0], box[1]), (box[2], box[3]),
-                                (0, 0, 255), 4)
-            img = self.convert_from_cv2_to_image(img)
+        flag = torch.round(prediction['class'][0]) == 1
+        label += "| Pred "
+        label += "Yes" if flag else 'No'
+        label += ' Prob {}'.format(prediction['class'][0])
+        if flag:
+            img = self.add_box(img, prediction['bbox'][0], target, (0, 0, 255))
         plt.title(label)
         plt.imshow(img)
         plt.show()
