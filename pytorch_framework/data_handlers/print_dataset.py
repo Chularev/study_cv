b1382dad9cc55976f6d1799509a6254cde3de24b
Chularev <chulgarev@gmail.com>
2022-03-29 04:53:16 +0700

    tmp

--- pytorch_framework/data_handlers/print_dataset.py
+++ pytorch_framework/data_handlers/print_dataset.py
@@ -3,13 +3,6 @@ import torch
 from viewer import Viewer
 from logger import Logger
 
-def mask_image( img, mask ):
-   newImg = img.copy()
-   newImg[:,:,0] = img[:,:,0] * mask[:,:]
-   newImg[:, :,1] = img[:, :,1] * mask[:, :]
-   newImg[:, :, 2] = img[:, :,2] * mask[:, :]
-   return newImg
-
 if __name__ == "__main__":
     datasets = get_datasets()
     train = datasets['train']
@@ -25,7 +18,7 @@ if __name__ == "__main__":
         image = viewer.add_title(img_orig, 'Original img')
         result.append(torch.from_numpy(image).permute(2, 0, 1).unsqueeze(0))
 
-        mask = mask_image(img_orig, mask)
+        mask = viewer.mask_image(img_orig, mask)
         mask = viewer.convert_from_image_to_cv2(mask)
         image = viewer.add_title(mask, 'Mask ')
         result.append(torch.from_numpy(image).permute(2, 0, 1).unsqueeze(0))
