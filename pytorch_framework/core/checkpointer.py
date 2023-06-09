1e7df6f964d6d5eb5fc380130cc907298be481a1
Alex <chulgarev@gmail.com>
2023-06-09 22:30:53 +0200

    added checkpointer

--- pytorch_framework/core/checkpointer.py
+++ pytorch_framework/core/checkpointer.py
@@ -1,8 +1,9 @@
 import os
+import torch
 
 from core.train_context import _TrainContext
 from core.train_param_enums import LoadStrategy, SaveStrategy, MetricType
-from helpers.constants import CHECKPOINT_DIR
+from helpers.constants import CHECKPOINT_FILE
 
 
 class Checkpointer:
@@ -10,25 +11,49 @@ class Checkpointer:
     def __init__(self, context: _TrainContext):
         self.c = context
         self.best_metric = 0
+        
     def load(self):
 
         if not os.path.exists(CHECKPOINT_DIR):
-            return
+            return False
 
         if self.c.load_strategy == LoadStrategy.NONE:
-            return
-        '''
-           if checkpoint_dir:
-        checkpoint = os.path.join(checkpoint_dir, "checkpoint")
-        model_state, optimizer_state = torch.load(checkpoint)
-        context.model.load_state_dict(model_state)
-        context.optimizer.load_state_dict(optimizer_state)
-        :return:
-        '''
-        return 0
+            return False
 
+        checkpoint = os.path.join(CHECKPOINT_FILE)
+        model_state, optimizer_state, metric = torch.load(checkpoint)
+
+        self.best_metric = metric
+
+        if self.c.load_strategy == LoadStrategy.MODEL_OPTIMIZER:
+            self.c.optimizer.load_state_dict(optimizer_state)
+
+        self.c.model.load_state_dict(model_state)
+
+        return True
+
+    def _save_on_disk(self, metric):
+        self.best_metric = metric
+        return 0
     def save(self):
         return 0
 
-    def is_finish(self):
+    def is_finish(self, metric):
+        if self.c.metric_type == metric:
+            if self.c.metric_value_stop <= metric:
+                self._save_on_disk(metric)
+                return True
+
+        if self.c.metric_value_stop >= metric:
+            self._save_on_disk(metric)
+            return True
+
         return False
+
+if __name__ == "__main__":
+    context = _TrainContext()
+    context.load_strategy = LoadStrategy.MODEL
+
+    CHECKPOINT_DIR = '/home/alex/workspace/experiments/best_model/'
+    checkopinter = Checkpointer(context)
+    print(checkopinter.load())
--- pytorch_framework/helpers/constants.py
+++ pytorch_framework/helpers/constants.py
@@ -7,7 +7,7 @@ IMG_DIR = ROOT_DIR + "/data/data/images"
 LABEL_DIR = ROOT_DIR + "/data/data/labels"
 TRAIN_CSV_FILE = ROOT_DIR + "/data/train.csv"
 VAL_CSV_FILE = ROOT_DIR + "/data/test.csv"
-CHECKPOINT_DIR = '/home/alex/workspace/experiments/best_model/'
+CHECKPOINT_FILE = '/home/alex/workspace/experiments/best_model/model.pth.tar'
 
 
 if __name__ == "__main__":
@@ -15,4 +15,5 @@ if __name__ == "__main__":
     print("IMG_DIR = ", IMG_DIR)
     print("LABEL_DIR = ", LABEL_DIR)
     print("TRAIN_CSV_FILE = ", TRAIN_CSV_FILE)
-    print("VAL_CSV_FILE", VAL_CSV_FILE)
+    print("VAL_CSV_FILE = ", VAL_CSV_FILE)
+    print('CHECKPOINT_FILE = ', CHECKPOINT_FILE)
