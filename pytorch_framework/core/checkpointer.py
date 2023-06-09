216246f6f10f8257b93ac4b7907e9bb7170f67ea
Alex <chulgarev@gmail.com>
2023-06-10 01:23:03 +0200

    tmp class checkpointer done but with bug

--- pytorch_framework/core/checkpointer.py
+++ pytorch_framework/core/checkpointer.py
@@ -14,14 +14,14 @@ class Checkpointer:
         
     def load(self):
 
-        if not os.path.exists(CHECKPOINT_DIR):
+        if not os.path.exists(CHECKPOINT_FILE):
             return False
 
         if self.c.load_strategy == LoadStrategy.NONE:
             return False
 
-        checkpoint = os.path.join(CHECKPOINT_FILE)
-        model_state, optimizer_state, metric = torch.load(checkpoint)
+       # checkpoint = os.path.join(CHECKPOINT_FILE)
+        model_state, optimizer_state, metric = torch.load(CHECKPOINT_FILE)
 
         self.best_metric = metric
 
@@ -34,26 +34,55 @@ class Checkpointer:
 
     def _save_on_disk(self, metric):
         self.best_metric = metric
-        return 0
-    def save(self):
-        return 0
 
-    def is_finish(self, metric):
-        if self.c.metric_type == metric:
+        checkpoint = {
+            "state_dict": self.c.model.state_dict(),
+            "optimizer": self.c.optimizer.state_dict(),
+            "metric": self.best_metric
+        }
+        torch.save(checkpoint, CHECKPOINT_FILE)
+
+    def _check_metric(self, metric):
+        if self.c.metric_type == MetricType.METRIC:
             if self.c.metric_value_stop <= metric:
-                self._save_on_disk(metric)
                 return True
 
+        # metric is loss
         if self.c.metric_value_stop >= metric:
-            self._save_on_disk(metric)
             return True
 
         return False
+
+    def save(self, metric):
+        if self.c.save_strategy == SaveStrategy.NONE:
+            return False
+
+        if self.c.save_strategy == SaveStrategy.BEST_MODEL_OPTIMIZER or self.c.save_strategy == SaveStrategy.BEST_MODEL:
+            if not self._check_metric(metric):
+                return False
+
+        checkpoint = {
+            'model': self.c.model,
+            'metric': self.best_metric
+        }
+
+        if self.c.save_strategy == SaveStrategy.BEST_MODEL_OPTIMIZER or self.c.save_strategy == SaveStrategy.MODEL_OPTIMIZER:
+            checkpoint['optimizer'] = self.c.optimizer
+
+        torch.save(checkpoint, CHECKPOINT_FILE)
+        return True
+
+    def is_finish(self):
+
+        result = self._check_metric(self.c.metric_value_stop)
+        if result:
+            self._save_on_disk(self.c.metric_value_stop)
+
+        return result
 
 if __name__ == "__main__":
     context = _TrainContext()
     context.load_strategy = LoadStrategy.MODEL
 
-    CHECKPOINT_DIR = '/home/alex/workspace/experiments/best_model/'
     checkopinter = Checkpointer(context)
     print(checkopinter.load())
