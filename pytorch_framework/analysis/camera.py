7c29b750494ceb5dfdd1a823b96e4224c3ab11e4
Alex <chulgarev@gmail.com>
2023-10-10 14:30:02 +0200

    added profile

--- pytorch_framework/analysis/camera.py
+++ pytorch_framework/analysis/camera.py
@@ -8,54 +8,56 @@ import cv2
 
 matplotlib.use('TKAgg')
 
-def loop(ax,freq,need, vid, predictor):
-    # Capture the video frame
-    # by frame
-    ret, frame = vid.read()
+class main_loop:
 
-    # Display the resulting frame
-    if need == freq:
-        need = 0
-        frame = predictor.opencv_img(frame)
+    @my_profile
+    def main(serlf):
 
-        # Display the image
-        ax.imshow(frame)
-        plt.pause(0.001)
-
-    need = need + 1
-
-    # frame  = cv2.resize(frame, (height, width), interpolation = cv2.INTER_AREA)
-    # cv2.imshow('frame', frame2)
-@my_profile
-def main():
-
-    predictor = Predictor('/home/alex/workspace/experiments/best_model/model_metric.pth.tar')
+        predictor = Predictor('/home/alex/workspace/experiments/best_model/model_metric.pth.tar')
 
 
-    # define a video capture object
-    vid = cv2.VideoCapture(0)
-    fig, ax = plt.subplots(1)
+        # define a video capture object
+        vid = cv2.VideoCapture(0)
+        fig, ax = plt.subplots(1)
 
-    freq = 5
-    need = freq
-    i = 0
-    while (True):
-        loop(ax,freq,need,vid,predictor)
-        # the 'q' button is set as the
-        # quitting button you may use any
-        # desired button of your choice
-        if i == 10:
-            die()
-        i = i + 1
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
+        freq = 5
+        need = freq
+        i = 0
+        while (True):
+            # Capture the video frame
+            # by frame
+            ret, frame = vid.read()
+
+            # Display the resulting frame
+            if need == freq:
+                need = 0
+                frame = predictor.opencv_img(frame)
+
+                # Display the image
+                ax.imshow(frame)
+                plt.pause(0.001)
+
+            need = need + 1
+
+            # frame  = cv2.resize(frame, (height, width), interpolation = cv2.INTER_AREA)
+            # cv2.imshow('frame', frame2)
+            # the 'q' button is set as the
+            # quitting button you may use any
+            # desired button of your choice
+            if i == 10:
+                break
+            i = i + 1
+
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
 
 
 
-    # After the loop release the cap object
-    vid.release()
-    # Destroy all the windows
-    cv2.destroyAllWindows()
+        # After the loop release the cap object
+        vid.release()
+        # Destroy all the windows
+        cv2.destroyAllWindows()
 
 if __name__ == "__main__":
-    main()
+    main = main_loop()
+    main.main()
