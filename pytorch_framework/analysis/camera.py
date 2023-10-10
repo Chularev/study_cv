e060e08da84e00dad3a0bb1065f31d945d7e9ae1
Alex <chulgarev@gmail.com>
2023-10-10 14:43:28 +0200

    refactoring

--- pytorch_framework/analysis/camera.py
+++ pytorch_framework/analysis/camera.py
@@ -10,51 +10,46 @@ matplotlib.use('TKAgg')
 
 class main_loop:
 
-    @my_profile
-    def main(serlf):
-
-        predictor = Predictor('/home/alex/workspace/experiments/best_model/model_metric.pth.tar')
-
+    def __init__(self):
+        self.predictor = Predictor('/home/alex/workspace/experiments/best_model/model_metric.pth.tar')
 
         # define a video capture object
-        vid = cv2.VideoCapture(0)
-        fig, ax = plt.subplots(1)
+        self.vid = cv2.VideoCapture(0)
+        self.fig, self.ax = plt.subplots(1)
 
-        freq = 5
-        need = freq
+        self.i_max = 10
+
+    @my_profile
+    def loop(self):
         i = 0
         while (True):
             # Capture the video frame
             # by frame
-            ret, frame = vid.read()
+            ret, frame = self.vid.read()
 
-            # Display the resulting frame
-            if need == freq:
-                need = 0
-                frame = predictor.opencv_img(frame)
 
-                # Display the image
-                ax.imshow(frame)
-                plt.pause(0.001)
+            frame = self.predictor.opencv_img(frame)
 
-            need = need + 1
+            # Display the image
+            self.ax.imshow(frame)
+            plt.pause(0.001)
 
             # frame  = cv2.resize(frame, (height, width), interpolation = cv2.INTER_AREA)
             # cv2.imshow('frame', frame2)
             # the 'q' button is set as the
             # quitting button you may use any
             # desired button of your choice
-            if i == 10:
+            if i == self.i_max:
                 break
             i = i + 1
 
             if cv2.waitKey(1) & 0xFF == ord('q'):
                 break
 
-
-
+    def main(self):
+        self.loop()
         # After the loop release the cap object
-        vid.release()
+        self.vid.release()
         # Destroy all the windows
         cv2.destroyAllWindows()
 
